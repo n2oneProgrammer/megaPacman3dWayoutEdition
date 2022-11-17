005b43aaec4fe9af1108be8d51941ed0a6dc7070
n2oneProgrammer <kubawojtasik70@gmail.com>
2022-11-17 19:21:17 +0100

    eeee

---
+++ vite.config.js
@@ -0,0 +1,11 @@
+/** @type {import('vite').UserConfig} */
+import { defineConfig } from "vite";
+
+export default defineConfig(({ command, mode, ssrBuild }) => {
+    if (command === "build") {
+        return {
+            base: "/megaPacman3dWayoutEdition/",
+        };
+    }
+    return {};
+});
